623725f34c2c2281cfa4f6879a35a9ea21bccb23
lollipop190 <191250190@smail.nju.edu.cn>
2021-11-14 11:28:06 +0800

    concurrency optimized finished

--- ITSC-SwiftUI/ITSC/newsContent.swift
+++ ITSC-SwiftUI/ITSC/newsContent.swift
@@ -9,62 +9,45 @@ import SwiftUI
 import SwiftSoup
 struct newsContent: View {
     let url: String
-    var document: Document? {
-        get async {
-        do {
-            let html = try String(contentsOf: URL(string: url)!)
-            let document = try SwiftSoup.parse(html)
-            return document
-            }catch {
-            print(error)
-            }
-            return nil
-        }
-        }
-    
+
     @State var titleA: String = ""
     @State var contentA: String = ""
     @State var timeA = ""
     @State var countA = ""
-    var title: String? {
-        get async {
-        try? await document?.select("h1.arti_title").text()
-        }
+    
+    func load() async -> (String, String, String, String) {
+        do {
+            let html = try String(contentsOf: URL(string: url)!)
+            let document = try SwiftSoup.parse(html)
+            let title = try document.select("h1.arti_title").text()
+            let content = try document.select("div.wp_articlecontent > p").text()
+            let time = try document.select("span.arti_update").text()
+            var s =  try document.select("span.WP_VisitCount").text()
+            s = "浏览次数：" + s;
+            return (title, content, time, s)
+            }catch {
+            print(error)
+            }
+            return ("","","","")
     }
-    var content: String? {
-        get async {
-        try? await document?.select("div.wp_articlecontent > p").text()
-        }
-    }
-    var time: String? {
-        get async {
-        try? await document?.select("span.arti_update").text()
-        }
-    }
-    var count: String? {
-        get async {
-        let s =  try? await document?.select("span.WP_VisitCount").text()
-        return "浏览次数：" + (s ?? "0")
-        }
-    }
+    
     var body: some View {
     
         VStack{
             Text(titleA)
                 .foregroundColor(.red)
                 .font(.system(size: 25))
-            Text(timeA + countA).padding().font(.system(size: 15))
+            Text(timeA + "   " + countA).padding().font(.system(size: 15))
             ScrollView{
                 Text(contentA).padding(.horizontal)
             }
        
         }.task {
-            let s = await [title!,time!, count!, content!]
-            titleA = s[0]
-            timeA = s[1]
-            countA = s[2]
-            contentA = s[3]
-        
+            let (a,b,c,d) = await load()
+            titleA = a
+            contentA = b
+            timeA = c
+            countA = d
         }
 
     }
